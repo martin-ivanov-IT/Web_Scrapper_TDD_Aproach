1df1f6160d2d8adfdf60e3890f22aa1e843fcd4b
martin-ivanov-IT <mar7i798@gmail.com>
2021-06-08 13:13:28 +0200

    add tests for WebScrapper

--- test/unit_tests/WebScrapperTest.py
+++ test/unit_tests/WebScrapperTest.py
@@ -20,6 +20,14 @@ def localMainPageSoup():
         return soup
 
 
+@pytest.fixture()
+def localFirstArticleSoup():
+    with open("firstArticleHTML.html", encoding='utf-8') as fp:
+        contents = fp.read()
+        soup = BeautifulSoup(contents, "html.parser")
+        return soup
+
+
 def test_WebScrapperClass(webScrapperDemo):
     assert isinstance(webScrapperDemo, WebScrapper)
 
@@ -46,10 +54,34 @@ def test_main_Page_soup(localMainPageSoup, webScrapperDemo):
     assert webScrapperDemo.soup.text == ws.soup.text
 
 
-def test_get_articles_urls(webScrapperDemo, localMainPageSoup):
+def test_get_articles_urls(localMainPageSoup):
     articlesUrlsTest = localMainPageSoup.find_all('a', class_='more-link')[:3]
     ws = WebScrapper("https://blog.bozho.net/")
     ws.makeDataDictionary()
     articlesUrls = ws.get_articles_urls(atr='a', classAtr='more-link', articlesNeeded=3)
 
     assert collections.Counter(articlesUrlsTest) == collections.Counter(articlesUrls)
+
+
+def test_get_content(localFirstArticleSoup):
+    text = localFirstArticleSoup.find('div', class_='post-content').get_text()
+    lines = (line.strip() for line in text.splitlines())
+    chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+    text = '\n'.join(chunk for chunk in chunks if chunk)
+    ws = WebScrapper("https://blog.bozho.net/blog/3733")
+    ws.makeDataDictionary()
+    assert text == ws.get_content('div', 'post-content')
+
+
+def test_get_title(localFirstArticleSoup):
+    title = localFirstArticleSoup.find('h1').text
+    ws = WebScrapper("https://blog.bozho.net/blog/3733")
+    ws.makeDataDictionary()
+    assert title == ws.get_title('h1')
+
+
+def test_get_date(localFirstArticleSoup):
+    date = localFirstArticleSoup.find("time", class_="entry-date published updated").text
+    ws = WebScrapper("https://blog.bozho.net/blog/3733")
+    ws.makeDataDictionary()
+    assert date == ws.get_date("time", "entry-date published updated")
