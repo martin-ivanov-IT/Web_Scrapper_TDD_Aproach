3faae88b0e2118801de4164afb62f22791f0d0b6
martin-ivanov-IT <mar7i798@gmail.com>
2021-06-10 01:14:13 +0200

    update

--- test/unit_tests/DataFormaterTest.py
+++ test/unit_tests/DataFormaterTest.py
@@ -1,20 +1,31 @@
 import pytest
 from module.data_formatter import DataFormatter
 import requests
+import csv
+import codecs
+import collections
 
 
 @pytest.fixture()
 def dataFormatterDemo():
     df = DataFormatter("https://blog.bozho.net/")
     df.fetchHtml()
+
     return df
+
+
+@pytest.fixture()
+def extractedFirstRow():
+    with codecs.open("firtArticleExtracted.csv", "r", 'utf-8') as fp:
+        csv_reader = csv.reader(fp)
+        return next(csv_reader)
 
 
 def test_DataFormatterClass(dataFormatterDemo):
     assert isinstance(dataFormatterDemo, DataFormatter)
 
 
-def test_check_fetchHtml(dataFormatterDemo):
+def test_fetchHtml(dataFormatterDemo):
     assert isinstance(dataFormatterDemo.html, requests.Response)
     assert dataFormatterDemo.html
 
@@ -25,3 +36,13 @@ def test_wrong_url():
 
     with pytest.raises(requests.exceptions.ConnectionError):
         ws.fetchHtml()
+
+
+def test_writeRowToCSV(dataFormatterDemo, extractedFirstRow):
+    with codecs.open("testWriter.csv", "w", "utf-8") as fp:
+        writer = csv.writer(fp)
+        dataFormatterDemo.writeRowToCSV(writer, "test_title", "test_date", "test_content")
+    fp.close()
+    with codecs.open("testWriter.csv", "r", "utf-8") as fp:
+        csv_reader = csv.reader(fp)
+        assert collections.Counter(next(csv_reader)) == collections.Counter(extractedFirstRow)
